a37d609565614cae8cb0bd6e8936082385e5ed89
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-09-08 17:12:57 +0300

    final

--- gallery-refs.js
+++
@@ -1,8 +0,0 @@
-export const refs = {
-    gallery: document.querySelector('.js-gallery'),
-    lightbox: document.querySelector('js-lightbox'),
-    lightboxImage: document.querySelector('.lightbox__image'),
-    lightboxBtn: document.querySelector("[data-action='close-lightbox']"),
-    lightboxOverlay: document.querySelector('.lightbox__overlay'),
-
-}
--- gallery.js
+++ gallery.js
@@ -1,4 +1,70 @@
+import galleryItems from '../app.js';
 
+// References
+
+const gallery = document.querySelector('.js-gallery');
+
+const container = document.querySelector('div.lightbox');
+const imageSource = document.querySelector('img.lightbox__image');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+const clearButton = document.querySelector('button[data-action="close-lightbox"]');
+
+
+
+
+
+ //CREATION OF THE GALLERY
+
+
+ function createGallery({ preview, original, description}, index) {
+     return `<li class = "gallery__item"><a class = "gallery__link" href = "${original}"><img class = "gallery__image" src='${preview}' data-sourse = '${original}' data-index = '${index}' alt='${description}'></a></li>`;
+}
+const markup = galleryItems.map(createGallery).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+ 
+
+
+
+
+
+// DELEGATION
+
+
+
+gallery.addEventListener('click', closeUPView);
+
+
+function closeUPView(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    } else {
+      
+        container.classList.add('is-open');
+        const imageRef = evt.target;
+        const largeImgURL = imageRef.dataset.sourse;
+        imageSource.src = largeImgURL;
+        imageSource.alt = imageRef.alt;
+
+        }  
+}
+
+
+//MODAL CLOSED
+
+
+
+clearButton.addEventListener('click', modalClose);
+lightboxOverlay.addEventListener('click', modalClose);
+
+function modalClose() {
+    container.classList.remove('is-open');
+     imageSource.src = '';
+     imageSource.alt = '';
+     imageSource.dataset.index = '';
+}
 
 
 
